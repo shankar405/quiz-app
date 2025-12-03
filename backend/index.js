9e953864bcd7cdb17b030efe84a7cec9b07d7a64
shankar405 <shankarmalviya067@gmail.com>
2025-12-03 22:36:26 +0530

    updated cors origin

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
- origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+ origin: ["http://localhost:5173", "http://127.0.0.1:5173,https://quiz-app-opal-eight.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
